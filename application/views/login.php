052461435b4c00b826e2db6d7e7824165924abe7
kenny-tech <kenny4real2001@gmail.com>
2018-11-01 00:20:02 -0700

    Create login

---
+++ application/views/login.php
@@ -0,0 +1,14 @@
+<div class="container">
+  <h2>Login</h2>
+  <form action="/action_page.php">
+    <div class="form-group">
+      <label for="email">Email:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+    </div>
+    <div class="form-group">
+      <label for="pwd">Password:</label>
+      <input type="password" class="form-control" id="password" placeholder="Enter password" name="password">
+    </div>
+    <button type="submit" class="btn btn-default">Submit</button>
+  </form>
+</div>
