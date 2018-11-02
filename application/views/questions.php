c5a79c8da2247ffbad5b50e896d4d1e6853ce160
kenny-tech <kenny4real2001@gmail.com>
2018-11-02 10:15:59 +0100

    Updated quiz questions

--- application/views/questions.php
+++ application/views/questions.php
@@ -1,5 +1,20 @@
 <?php include('header.php'); ?>
+<script type="text/javascript">
+var val;
+	function ensure()
+	{
+		var agree=confirm("Are you sure you want to end the quiz?")
+		if(agree){
+  		return true
+  		document.form1.submit()
+		}
+		else
+		  return false
+	}
+</script>
+
 <?php include('navigation.php') ?>
+
 <div class="container">
   <h2>Quiz</h2>
   <?php
@@ -11,20 +26,29 @@
   </div>
   <?php } ?>
   <h3>Questions</h3>
-  <?php 
-      if(isset($result)) {
-        foreach($result as $row) {
-          $id = $row->id;
-          $question = $row->question;
-				  $option1 = $row->option1;
-				  $option2 = $row->option2;
-				  $option3 = $row->option3;
-				  $option4 = $row->option4;
+  <?php
+    $num = 0;
+    if(isset($result)) {
+    foreach($result as $row) {
+      $id = $row->id;
+      $question = $row->question;
+			$option1 = $row->option1;
+			$option2 = $row->option2;
+			$option3 = $row->option3;
+			$option4 = $row->option4;
+      ++$num;
   ?>
-  <p><strong><?php echo $id; ?>&nbsp;<?php echo $question; ?></strong></p>
-  <input type="radio" value="<?php echo $option1 ?>" />&nbsp;<?php echo $option1; ?>
-  <input type="radio" value="<?php echo $option2 ?>" />&nbsp;<?php echo $option2; ?>
-  <input type="radio" value="<?php echo $option3 ?>" />&nbsp;<?php echo $option3; ?>
-  <input type="radio" value="<?php echo $option4 ?>" />&nbsp;<?php echo $option4; ?>
+  <form action="" method="post" name="form1">
+  <div class="card" style="width: 50rem;">
+    <div class="card-body">
+      <h5 class="card-title"><?php echo $id; ?>&nbsp;<?php echo $question; ?></h5>
+      <p class="card-text"><input type="radio" name="ans[<?php echo $num; ?>]" value="<?php echo $option1 ?>" />&nbsp;<?php echo $option1; ?></p>
+      <p class="card-text"><input type="radio" name="ans[<?php echo $num; ?>]" value="<?php echo $option2 ?>" />&nbsp;<?php echo $option2; ?></p>
+      <p class="card-text"><input type="radio" name="ans[<?php echo $num; ?>]" value="<?php echo $option3 ?>" />&nbsp;<?php echo $option3; ?></p>
+      <p class="card-text"><input type="radio" name="ans[<?php echo $num; ?>]" value="<?php echo $option4 ?>" />&nbsp;<?php echo $option4; ?></p>
+    </div>
+  </div>
+</form>
   <?php } } ?>
+  <p align="center"><input type="submit" name="submit" value="End quiz" class="btn btn-primary" onclick="ensure()"/></p>
 <?php include('footer.php'); ?>
